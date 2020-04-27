e8f10d0de96b2d174fcdaa303e7d7bbfde10bd2b
Saket Penurkar <saket.penurkar@colorado.edu>
2020-04-26 21:51:33 -0700

    Working Codes for Sprint 3

--- base_external/rootfs_overlay/bin/MQTT_Server/Server.py
+++ base_external/rootfs_overlay/bin/MQTT_Server/Server.py
@@ -1,24 +1,50 @@
-#Filename: Server-Subscriber.py
-#Autheo: Sankalp Pund
-#Description: This python script works as mqtt server-subscriber
-#Reference: https://github.com/eclipse/paho.mqtt.python
-
 import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.client as mqtt
+import datetime
 
 # This is the Subscriber
-print("This is Server")
+
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("topic/test")
 
+datetime_object = datetime.datetime.now() 
+
 def on_message(client, userdata, msg):
   a=msg.payload.decode()
-  print("Received Message from Client is - - ->: '%s'" %a)	
-  if a == "Sending test data to Server...":
-    print("Success! Test data has been received at server from client side")
-# client.disconnect()
-    
+  print("Payload is: %s" %(a))
+  data_identifier = a[0]
+  print("Data Identifier is: %s" %(data_identifier))
+  if data_identifier == "T":
+      try:
+          data_to_write = a[1:]
+          print("Data to write is %s" %(data_to_write))
+          file_handler = open('/usr/htdocs/temperature.txt', 'a')
+          file_handler.write(data_to_write)
+          file_handler.write("\n")
+          file_handler.close()
+          #print(datetime_object)
+          #print("Current Temperature is - - ->: %s" %a)
+      except Exception as e:
+          print(e)
+  elif data_identifier == "H":
+      try:
+          data_to_write = a[1:]
+          print("Data to write is %s" %(data_to_write))
+          file_handler = open('/usr/htdocs/humidity.txt', 'a')
+          print("Writing to file")
+          file_handler.write(data_to_write)
+          file_handler.write("\n")
+          print("Closing file")
+          file_handler.close()
+          #print(datetime_object)
+          #print("Current Humidity is - - ->: %s" %a)
+      except Exception as e:
+          print(e)
+  else:
+      print("T not found")
+
+ 
 client = mqtt.Client()
 client.connect("localhost",1883,60)
 
